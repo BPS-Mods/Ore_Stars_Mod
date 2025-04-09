d5dca24ca5f22ae4fdeb7771052e0cc09873bdde
SelimGOZ <gselim504@gmail.com>
2025-04-09 12:28:55 -0700

    1.20.4 update

--- fabric/src/main/java/io/thecheese/orestars/OreStars.java
+++ fabric/src/main/java/io/thecheese/orestars/OreStars.java
@@ -1,18 +1,18 @@
 package io.thecheese.orestars;
 
 import io.thecheese.orestars.item.ModItemGroups;
-import io.thecheese.orestars.item.ModItemsFabric;
+import io.thecheese.orestars.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class OreStarsFabric implements ModInitializer {
+public class OreStars implements ModInitializer {
 	public static final String MOD_ID = "orestars";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 		ModItemGroups.registerItemGroups();
-		ModItemsFabric.registerModItems();
+		ModItems.registerModItems();
 	}
 }
