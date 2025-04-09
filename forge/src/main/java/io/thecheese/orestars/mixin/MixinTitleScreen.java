9be9d0c53ebbd969bf2d331caf269eb31fbd0800
SelimGOZ <gselim504@gmail.com>
2025-04-09 12:44:35 -0700

    1.20.1 update

--- forge/src/main/java/io/thecheese/orestars/mixin/MixinTitleScreen.java
+++ forge/src/main/java/io/thecheese/orestars/mixin/MixinTitleScreen.java
@@ -9,12 +9,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class MixinTitleScreenForge {
+public class MixinTitleScreen {
 
     @Inject(at = @At("HEAD"), method = "init()V")
     private void init(CallbackInfo info) {
 
         Constants.LOG.info("If you see this. Hi :)");
-        Constants.LOG.info("MC Version: {}", Minecraft.getInstance().getVersionType());
     }
 }
