30cdd7fa27e7a165d96594f1f9e91dce024f1337
SelimGOZ <gselim504@gmail.com>
2025-04-09 12:13:18 -0700

    1.20 updates

--- fabric/src/main/java/io/thecheese/orestars/item/ModItemGroups.java
+++ fabric/src/main/java/io/thecheese/orestars/item/ModItemGroups.java
@@ -1,6 +1,6 @@
 package io.thecheese.orestars.item;
 
-import io.thecheese.orestars.OreStarsFabric;
+import io.thecheese.orestars.OreStars;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
@@ -11,23 +11,23 @@ import net.minecraft.util.Identifier;
 
 public class ModItemGroups {
     public static final ItemGroup ORESTAR_GROUP = Registry.register(Registries.ITEM_GROUP,
-            new Identifier(OreStarsFabric.MOD_ID, "orestars_group"),
+            new Identifier(OreStars.MOD_ID, "orestars_group"),
             FabricItemGroup.builder().displayName(Text.translatable("itemgroup.orestars_group"))
-                    .icon(() -> new ItemStack(ModItemsFabric.IronStar)).entries((displayContext, entries) -> {
-                        entries.add(ModItemsFabric.IronStar);
-                        entries.add(ModItemsFabric.GoldStar);
-                        entries.add(ModItemsFabric.EmeraldStar);
-                        entries.add(ModItemsFabric.DiamondStar);
-                        entries.add(ModItemsFabric.CopperStar);
-                        entries.add(ModItemsFabric.CoalStar);
-                        entries.add(ModItemsFabric.AmethystStar);
-                        entries.add(ModItemsFabric.LapisLazuliStar);
-                        entries.add(ModItemsFabric.NetheriteStar);
-                        entries.add(ModItemsFabric.QuartzStar);
-                        entries.add(ModItemsFabric.RedstoneStar);
+                    .icon(() -> new ItemStack(ModItems.IronStar)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.IronStar);
+                        entries.add(ModItems.GoldStar);
+                        entries.add(ModItems.EmeraldStar);
+                        entries.add(ModItems.DiamondStar);
+                        entries.add(ModItems.CopperStar);
+                        entries.add(ModItems.CoalStar);
+                        entries.add(ModItems.AmethystStar);
+                        entries.add(ModItems.LapisLazuliStar);
+                        entries.add(ModItems.NetheriteStar);
+                        entries.add(ModItems.QuartzStar);
+                        entries.add(ModItems.RedstoneStar);
                     }).build());
 
     public static void registerItemGroups() {
-        OreStarsFabric.LOGGER.info("Registering Item Group for " + OreStarsFabric.MOD_ID);
+        OreStars.LOGGER.info("Registering Item Group for " + OreStars.MOD_ID);
     }
 }
